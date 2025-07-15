78c4b129703fad78568c06019cd0414231b9e1be
mohitgarg05 <mgarg6858@gmail.com>
2025-07-15 19:58:19 +0530

    changes

--- src/app/_components/ProductCard.jsx
+++ src/app/_components/ProductCard.jsx
@@ -30,13 +30,14 @@ function ProductCard({ id, img, name, discountedPrice = 100, originalPrice = 200
             <div className='flex flex-col mt-2 text-zinc-800'>
                 <p className='font-semibold text-base'>{name}</p>
                 <span className='text-orange-600 font-semibold text-lg'><span className='text-gray-500 text-sm line-through'>{`₹${originalPrice}`}</span> {`₹${discountedPrice}`}</span>
-                <span className={`text-sm font-medium ${stock === 0 ? 'text-red-600' : 'text-green-700'}`}>{stock === 0 ? 'Out of Stock' : `In Stock: ${stock}`}</span>
+                {/* <span className={`text-sm font-medium ${stock === 0 ? 'text-red-600' : 'text-green-700'}`}>{stock === 0 ? 'Out of Stock' : `In Stock: ${stock}`}</span> */}
                 <button
                     className='w-full py-1 text-white border border-zinc-900 bg-zinc-900 transition-colors ease-in-out duration-300 hover:text-orange-600 hover:bg-white hover:border-orange-600 disabled:bg-gray-400 disabled:text-gray-700 disabled:cursor-not-allowed'
                     onClick={handleAddToCart}
-                    disabled={stock === 0}
+                    // disabled={stock === 0}
                 >
-                    {stock === 0 ? 'Out of Stock' : 'ADD TO CART'}
+                    {/* {stock === 0 ? 'Out of Stock' : 'ADD TO CART'} */}
+                    ADD TO CART
                 </button>
             </div>
         </div>
